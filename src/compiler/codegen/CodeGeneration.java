45f06437e34c4f3707d4709bdc15609304385191
lapestand <lapestand@gmail.com>
2021-06-21 08:36:14 +0300

    Code generation support for assign, read and write statements added.

--- src/compiler/codegen/CodeGeneration.java
+++ src/compiler/codegen/CodeGeneration.java
@@ -21,6 +21,8 @@ public class CodeGeneration {
     private int pc;
     private boolean check;
     private Helper helper;
+    private String notReadyStatement;
+    private int tempCount;
 
     public CodeGeneration(int pc) {
         this.codeLines = new ArrayList<String>();
@@ -34,8 +36,18 @@ public class CodeGeneration {
 
         this.helper = new Helper();
         this.check = true;
+        this.notReadyStatement = new String("\t*********THIS STATEMENT CURRENTLY NOT WORKING*********");
+        this.tempCount = 0;
     }
 
+
+    public void print() {
+        System.out.println();
+        for (String codeLine : this.codeLines) {
+            System.out.println(codeLine);
+        }
+    }
+    
     public void parse(List<Statement> statements){
         if (this.check) {
             System.out.println("CodeGeneration parsing started.");
@@ -51,92 +63,315 @@ public class CodeGeneration {
     private List <String> parseStatement(Statement statement) {     
         List <String> innerCodeLines = new ArrayList<String>();
 
-        ReadStatement tmpRead;
-        WriteStatement tmpWrite;
-        AssignStatement tmpAssign;
-        IfStatement tmpIf;
-        RepeatStatement tmpRepeat;
-
-
         if (statement.ST == Statement.READ_ST) {
-
-            tmpRead = (ReadStatement)statement;
-            innerCodeLines.add(
-                new String(
-                    "IN " + Integer.toString(variables.size() + 1) + ",0,0" 
-                    )
-            );
-            this.variables.put(tmpRead.id(), variables.size() + 1);
+            innerCodeLines.add(this.parseRead(statement));
         }
 
         else if (statement.ST == Statement.WRITE_ST) {
-            tmpWrite = (WriteStatement)statement;
-            innerCodeLines.add(
-                new String(
-                    "OUT " + Integer.toString(this.variables.get(tmpWrite.expr())) + ",0,0"
-                    )
-            );
+            innerCodeLines.add(this.parseWrite(statement));
         }
 
         else if (statement.ST == Statement.ASSIGN_ST) {
-            tmpAssign = (AssignStatement)statement;
-            if (tmpAssign.assignFrom.size() == 1) {
-                if (this.helper.isNum(tmpAssign.assignFrom.get(0))){
-                    // a := 1
-                    if (this.variables.containsKey(tmpAssign.assignTo)) {
-                        innerCodeLines.add(
-                            new String("LDC " + this.variables.get(tmpAssign.assignTo) + "," + tmpAssign.assignFrom.get(0) + "(0)")
-                        );
-                    } else {
-                        this.variables.put(tmpAssign.assignTo, this.variables.size() + 1);
-                        innerCodeLines.add(
-                            new String("LDC " + this.variables.get(tmpAssign.assignTo) + "," +  tmpAssign.assignFrom.get(0) + "(0)")
-                        );
-                    }
-                }
-                else {
-                    // a := b
-                    innerCodeLines.add(
-                        new String("LDA " + this.variables.get(tmpAssign.assignTo) + ",0(" + this.variables.get(tmpAssign.assignFrom.get(0)) + ")")
-                    );
-                }
-            } else {
-                innerCodeLines.add(
-                    new String("\t*********THIS ASSIGN CURRENTLY NOT WORKING*********")
-                );
-                /*
-                if (check) {
-                    // a := 1 + 2
-                } else if () {
-                    // a := 1 + b || b + 1
-                }
-                */
-            }
+            innerCodeLines.addAll(this.parseAssign(statement));
         }
 
         else if (statement.ST == Statement.REPEAT_ST) {
-            tmpRepeat = (RepeatStatement)statement;
-
-            innerCodeLines.add(
-                new String("\t*********REPEAT CURRENTLY NOT WORKING*********")
-            );
+            innerCodeLines.addAll(this.parseRepeat(statement));
         }
 
         else if (statement.ST == Statement.IF_ST) {
-            tmpIf = (IfStatement)statement;
-            innerCodeLines.add(
-                new String("\t*********IF CURRENTLY NOT WORKING*********")
-            );
+            innerCodeLines.addAll(this.parseIf(statement));
         }
 
         return innerCodeLines;
     }
 
-    public void print() {
-        System.out.println();
-        for (String codeLine : this.codeLines) {
-            System.out.println(codeLine);
-        }
+    private List <String> parseRepeat(Statement statement) {
+        RepeatStatement tmpRepeat = (RepeatStatement)statement;
+        List <String> codeLines = new ArrayList <String>();
+        codeLines.add(this.notReadyStatement + " (REPEAT)");
+        return codeLines;
     }
+
+    private List <String> parseIf(Statement statement) {
+        IfStatement tmpIf = (IfStatement)statement;
+        List <String> codeLines = new ArrayList <String>();
+        codeLines.add(this.notReadyStatement + " (IF)");
+        return codeLines;
+    }
+
+
+    private List <String> parseAssign(Statement statement) {
+        AssignStatement tmpAssign = (AssignStatement)statement;
+        List <String> codeLines = new ArrayList <String>();
+
+        if (tmpAssign.assignFrom.size() == 1) {
+
+            if (this.helper.isNum(tmpAssign.assignFrom.get(0))){
+                // a := 1
+                codeLines.add(this.assignConstant(tmpAssign.assignTo, tmpAssign.assignFrom.get(0)));
+            }
+            else {
+                // a := b
+                codeLines.add(this.assignVariable(tmpAssign.assignTo, tmpAssign.assignFrom.get(0)));
+            }
+
+        } else if (tmpAssign.assignFrom.size() == 3) {
+            
+            if (tmpAssign.assignFrom.get(1).equals("+")) {
+                codeLines.addAll(this.addExpr(tmpAssign.assignTo, tmpAssign.assignFrom.get(0), tmpAssign.assignFrom.get(2)));
+            } else if (tmpAssign.assignFrom.get(1).equals("-")) {
+                codeLines.addAll(this.subExpr(tmpAssign.assignTo, tmpAssign.assignFrom.get(0), tmpAssign.assignFrom.get(2)));
+            } else if (tmpAssign.assignFrom.get(1).equals("*")) {
+                codeLines.addAll(this.mulExpr(tmpAssign.assignTo, tmpAssign.assignFrom.get(0), tmpAssign.assignFrom.get(2)));
+            } else if (tmpAssign.assignFrom.get(1).equals("/")) {
+                codeLines.addAll(this.divExpr(tmpAssign.assignTo, tmpAssign.assignFrom.get(0), tmpAssign.assignFrom.get(2)));
+            } else {
+                codeLines.add(this.notReadyStatement);
+            }
+
+        } else {
+            codeLines.add(this.notReadyStatement);
+        }
+        return codeLines;
+    }
+
+    private String parseWrite(Statement statement) {
+        WriteStatement tmpWrite = (WriteStatement)statement;
+        return new String("OUT " + Integer.toString(this.variables.get(tmpWrite.expr())) + ",0,0");
+    }
+
+    private String parseRead(Statement statement) {
+        int reg;
+        ReadStatement tmpRead = (ReadStatement)statement;    
+        reg = this.put(tmpRead.id());
+        return new String("IN " + reg + ",0,0" );
+    }
+
+    private String assignVariable(String assignTo, String assignFrom) {
+        String codeLine = new String();
+        int reg;
+
+        if (!this.variables.containsKey(assignTo)) { reg = this.put(assignTo);}
+        else { reg = this.variables.get(assignTo); }
+
+        codeLine = "LDA " + reg + ",0(" + this.variables.get(assignFrom) + ")";
+        return codeLine;
+    }
+
+    private String assignConstant(String assignTo, String assignFrom) {
+        String codeLine = new String();
+        int reg;
+
+        if (!this.variables.containsKey(assignTo)) { reg = this.put(assignTo);}
+        else { reg = this.variables.get(assignTo); }
+
+        codeLine = "LDC " + reg + "," +  assignFrom + "(0)";
+        return codeLine;
+    }
+
+    private List <String> divExpr(String assignTo, String assignFrom1, String assignFrom2) {
+        List <String> codeLines = new ArrayList <String>();
+
+        // a := 3 / 2
+        if (this.helper.isNum(assignFrom1) && this.helper.isNum(assignFrom2)) {
+            String assignFrom = new String();
+            assignFrom = Integer.toString(Integer.parseInt(assignFrom1) / Integer.parseInt(assignFrom2));
+            codeLines.add(this.assignConstant(assignTo, assignFrom));
+        }
+
+        // a := b / 1
+        else if (this.helper.isID(assignFrom1) && this.helper.isNum(assignFrom2)) {
+            String tempRegId = this.createNewTempRegister();
+            codeLines.add(this.assignConstant(tempRegId, assignFrom2));
+            codeLines.add(this.div(assignTo, assignFrom1, tempRegId));
+        }
+        
+        // a := 1 / b
+        else if (this.helper.isNum(assignFrom1) && this.helper.isID(assignFrom2)) {
+            String tempRegId = this.createNewTempRegister();
+            codeLines.add(this.assignConstant(tempRegId, assignFrom1));
+            codeLines.add(this.div(assignTo, assignFrom2, tempRegId));
+        }
+
+        // a := b / c
+        else if (this.helper.isID(assignFrom1) && this.helper.isID(assignFrom2)) {
+            codeLines.add(this.div(assignTo, assignFrom1, assignFrom2));
+        }
+
+        else { codeLines.add(this.notReadyStatement); }
+
+        return codeLines;
+    }
+
+    private List <String> mulExpr(String assignTo, String assignFrom1, String assignFrom2) {
+        List <String> codeLines = new ArrayList <String>();
+
+        
+        if (this.helper.isNum(assignFrom1) && this.helper.isNum(assignFrom2)) {
+            // a := 3 * 2
+            String assignFrom = new String();
+            assignFrom = Integer.toString(Integer.parseInt(assignFrom1) * Integer.parseInt(assignFrom2));
+            codeLines.add(this.assignConstant(assignTo, assignFrom));
+        }
+
+        // a := b * 1
+        else if (this.helper.isID(assignFrom1) && this.helper.isNum(assignFrom2)) {
+            String tempRegId = this.createNewTempRegister();
+            codeLines.add(this.assignConstant(tempRegId, assignFrom2));
+            codeLines.add(this.mul(assignTo, assignFrom1, tempRegId));
+        }
+        
+        // a := 1 * b
+        else if (this.helper.isNum(assignFrom1) && this.helper.isID(assignFrom2)) {
+            String tempRegId = this.createNewTempRegister();
+            codeLines.add(this.assignConstant(tempRegId, assignFrom1));
+            codeLines.add(this.mul(assignTo, assignFrom2, tempRegId));
+        }
+
+        // a := b * c
+        else if (this.helper.isID(assignFrom1) && this.helper.isID(assignFrom2)) {
+            codeLines.add(this.mul(assignTo, assignFrom1, assignFrom2));
+        }
+
+        else { codeLines.add(this.notReadyStatement); }
+
+        return codeLines;
+    }
+
+    private List <String> subExpr(String assignTo, String assignFrom1, String assignFrom2) {
+        List <String> codeLines = new ArrayList <String>();
+
+        
+        if (this.helper.isNum(assignFrom1) && this.helper.isNum(assignFrom2)) {
+            // a := 3 - 2
+            String assignFrom = new String();
+            assignFrom = Integer.toString(Integer.parseInt(assignFrom1) - Integer.parseInt(assignFrom2));
+            codeLines.add(this.assignConstant(assignTo, assignFrom));
+        }
+
+        // a := b - 1
+        else if (this.helper.isID(assignFrom1) && this.helper.isNum(assignFrom2)) {
+            String tempRegId = this.createNewTempRegister();
+            codeLines.add(this.assignConstant(tempRegId, assignFrom2));
+            codeLines.add(this.sub(assignTo, assignFrom1, tempRegId));
+        }
+        
+        // a := 1 - b
+        else if (this.helper.isNum(assignFrom1) && this.helper.isID(assignFrom2)) {
+            String tempRegId = this.createNewTempRegister();
+            codeLines.add(this.assignConstant(tempRegId, assignFrom1));
+            codeLines.add(this.sub(assignTo, assignFrom2, tempRegId));
+        }
+
+        // a := b - c
+        else if (this.helper.isID(assignFrom1) && this.helper.isID(assignFrom2)) {
+            codeLines.add(this.sub(assignTo, assignFrom1, assignFrom2));
+        }
+
+        else { codeLines.add(this.notReadyStatement); }
+
+        return codeLines;
+    }
+
+    private List <String> addExpr(String assignTo, String assignFrom1, String assignFrom2) {
+        List <String> codeLines = new ArrayList <String>();
+
+        // a := 1 + 2
+        if (this.helper.isNum(assignFrom1) && this.helper.isNum(assignFrom2)) {
+            String assignFrom = new String();
+            assignFrom = Integer.toString(Integer.parseInt(assignFrom1) + Integer.parseInt(assignFrom2));
+            codeLines.add(this.assignConstant(assignTo, assignFrom));
+        }
+
+        // a := b + 1
+        else if (this.helper.isID(assignFrom1) && this.helper.isNum(assignFrom2)) {
+            String tempRegId = this.createNewTempRegister();
+            codeLines.add(this.assignConstant(tempRegId, assignFrom2));
+            codeLines.add(this.add(assignTo, assignFrom1, tempRegId));
+        }
+        
+        // a := 1 + b
+        else if (this.helper.isNum(assignFrom1) && this.helper.isID(assignFrom2)) {
+            String tempRegId = this.createNewTempRegister();
+            codeLines.add(this.assignConstant(tempRegId, assignFrom1));
+            codeLines.add(this.add(assignTo, assignFrom2, tempRegId));
+        }
+
+        // a := b + c
+        else if (this.helper.isID(assignFrom1) && this.helper.isID(assignFrom2)) {
+            codeLines.add(this.add(assignTo, assignFrom1, assignFrom2));
+        }
+
+        else { codeLines.add(this.notReadyStatement); }
+
+        return codeLines;
+    }
+
+    private String add(String res, String add1, String add2) {
+        // a := b + c
+        String codeLine = new String();
+        int reg;
+
+        if (!this.variables.containsKey(res)) { reg = this.put(res); }
+        else { reg = this.variables.get(res); }
+        
+        codeLine = "ADD " + reg + "," + this.variables.get(add1) + "," + this.variables.get(add2);
+        return codeLine;
+    }
+
+    private String sub(String res, String sub1, String sub2) {
+        // a := b - c
+        String codeLine = new String();
+        int reg;
+        if (!this.variables.containsKey(res)) { reg = this.put(res); }
+        else { reg = this.variables.get(res); }
+        
+        codeLine = "SUB " + reg + "," + this.variables.get(sub1) + "," + this.variables.get(sub2);
+        return codeLine;
+    }
+
+    private String mul(String res, String mul1, String mul2) {
+        // a := b * c
+        String codeLine = new String();
+        int reg;
+        if (!this.variables.containsKey(res)) { reg = this.put(res); }
+        else { reg = this.variables.get(res); }
+        
+        codeLine = "MUL " + reg + "," + this.variables.get(mul1) + "," + this.variables.get(mul2);
+        return codeLine;
+    }
+
+    private String div(String res, String div1, String div2) {
+        // a := b / c
+        String codeLine = new String();
+        int reg;
+        if (!this.variables.containsKey(res)) { reg = this.put(res); }
+        else { reg = this.variables.get(res); }
+        
+        codeLine = "DIV " + reg + "," + this.variables.get(div1) + "," + this.variables.get(div2);
+        return codeLine;
+    }
+
+    private String createNewTempRegister() {
+        String newId = new String();
+        do {
+            newId = "temp" + Integer.toString(this.tempCount);
+            this.tempCount++;
+        } while (this.variables.containsKey(newId));
+        this.put(newId);
+        
+        return newId;
+    }
+
+    private int put(String id) {
+        if (this.variables.size() + 1 == this.pc) {
+            this.variables.put("PC", this.variables.size() + 1);
+        }
+        this.variables.put(id, this.variables.size() + 1);
+        return this.variables.size();
+    }
+
+    
 
 }
